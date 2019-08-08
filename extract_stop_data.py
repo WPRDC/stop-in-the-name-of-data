d52f4af4d4380acb49b14e39bc0170bc8c08a410
drw <drw@users.noreply.github.com>
2019-08-08 12:24:31 -0400

    Add line breaks to log output

--- extract_stop_data.py
+++ extract_stop_data.py
@@ -172,7 +172,7 @@ class StopUseSchema(pl.BaseSchema):
             if route_code not in missing_route_codes:
                 missing_route_codes[route_code] += 1
                 if route_code is None or len(route_code) >= 3:
-                    error_message = "   ** No real route designation found for route value {}. **".format(route_code)
+                    error_message = "   ** No real route designation found for route value {}. **\n".format(route_code)
                     print(error_message)
                     with open('missing_routes.log', 'a') as o:
                         o.write(error_message)
