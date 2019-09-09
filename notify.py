87fde528dd58dc9e13616017ae960ea4c3bf18ac
drw <drw@users.noreply.github.com>
2019-09-09 10:31:12 -0400

    Update send_to_slack function to accommodate all parameters sent by call in extract_stop_data.py

--- notify.py
+++ notify.py
@@ -1,26 +1,30 @@
+from parameters.remote_parameters import webhook_url
 
-def send_to_slack(message):
-    """This script sends the given message to the #notifications channel on the WPRDC Slack thing.
-    Note that this shouldn't be heavily used (e.g., for reporting every error a script encounters)
-    as API limits are a consideration. This script IS suitable for running when a script-terminating
-    exception is caught, so that you can report the irregular termination of an ETL script."""
+def send_to_slack(message,username=None,channel=None,icon=None):
+    """This script sends the given message to a particular channel on
+    Slack, as configured by the webhook_url. Note that this shouldn't
+    be heavily used (e.g., for reporting every error a script
+    encounters) as API limits are a consideration. This script IS
+    suitable for running when a script-terminating exception is caught,
+    so that you can report the irregular termination of an ETL script."""
 
-    import os, json, requests
+    import os, re, json, requests
     import socket
-    from parameters.remote_parameters import webhook_url
-
     IP_address = socket.gethostbyname(socket.gethostname())
+    hostname = re.sub(".local","",socket.gethostname())
     name_of_current_script = os.path.basename(__file__)
 
-    caboose = "(Sent from {} running on a computer at {}.)".format(name_of_current_script, IP_address)
+    caboose = "(Sent from {} running on a computer called {} at {}.)".format(name_of_current_script, hostname, IP_address)
     # Set the webhook_url to the one provided by Slack when you create the webhook at https://my.slack.com/services/new/incoming-webhook/
-    #webhook_url = 'https://hooks.slack.com/services/SOMESTRING/OTHERSTRING' # Webhook to send messages to.
     slack_data = {'text': message + " " + caboose}
     slack_data['username'] = 'TACHYON'
-    #To send this as a direct message instead, us the following line. 
-    #slack_data['channel"] = '@username'
-    slack_data['icon_emoji'] = ':coffin:' #':tophat:' # ':satellite_antenna:'
-
+    if username is not None:
+        slack_data['username'] = username
+    #To send this as a direct message instead, use the following line.
+    if channel is not None:
+        slack_data['channel'] = channel
+    if icon is not None:
+        slack_data['icon_emoji'] = icon #':coffin:' #':tophat:' # ':satellite_antenna:'
     response = requests.post(
         webhook_url, data=json.dumps(slack_data),
         headers={'Content-Type': 'application/json'}
@@ -31,6 +35,6 @@ def send_to_slack(message):
             % (response.status_code, response.text)
         )
 
-if __name__ == "__main__":
-    msg = "Time to make the donuts, fool!" 
+if __name__ == '__main__':
+    msg = "No sir, away! A papaya war is on!"
     send_to_slack(msg)
